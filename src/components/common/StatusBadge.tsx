c91745cbb269bc01a5ed7fce0c1486b92dd70052
thakuramit79 <thakuramit79@gmail.com>
2025-06-06 22:16:38 +0530

    Fix StatusBadge undefined status error

--- src/components/common/StatusBadge.tsx
+++ src/components/common/StatusBadge.tsx
@@ -57,7 +57,7 @@ export function StatusBadge({ status, className }: StatusBadgeProps) {
         className
       )}
     >
-      {status.replace('_', ' ')}
+      {(status || '').replace('_', ' ')}
     </span>
   );
 }
